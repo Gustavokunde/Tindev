a32cbc3cd208adce58d454ce6516a6487b401151
GustavoKunde <gustavokunde@gmail.com>
2020-02-24 13:00:22 -0300

    finalizando a aplicação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const DevController = require("./controllers/DevController");
 const LikeController = require("./controllers/LikeController");
+const DislikeController = require("./controllers/DislikeController");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {});
-
+routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
 routes.post("/devs/:devsId/likes", LikeController.store);
+routes.post("/devs/:devsId/likes", DislikeController.store);
 
 module.exports = routes;
